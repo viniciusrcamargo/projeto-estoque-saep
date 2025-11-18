05ac7f5638b0faff0b48af30a8ca8851ef3a5ee1
Vinicius <Viniciusr.camargo@gmail.com>
2025-11-18 14:32:25 -0300

    Adaptando sistema para vercel

--- bd.js
+++ bd.js
@@ -1,16 +1,12 @@
 import pkg from 'pg';
 import dotenv from 'dotenv';
 dotenv.config();
-//[pause]
+
 const { Pool } = pkg;
-//[pause]
+
 const bd = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL, 
+  ssl: { rejectUnauthorized: false } 
 });
 
-//[pause]
 export default bd;
