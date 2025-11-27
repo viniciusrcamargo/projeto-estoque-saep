8736b6f23c464bb199803366aaf6df0589fc7fdd
Vinicius <Viniciusr.camargo@gmail.com>
2025-11-27 14:40:31 -0300

    Projeto Saep Vercel

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(session({
 }));
 
 function checkAuth(req, res, next) {
-  if (req.session.usuario) return next();
+  if (req.session.usuari) return next();
   res.redirect('/auth/login');
 }
 
